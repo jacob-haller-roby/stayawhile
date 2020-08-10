88511c1c526160c3bade7b576889647496e45eb2
Jacob Haller-Roby <jroby@staywell.com>
2020-08-10 01:40:02 -0700

    prevent command duplicates

--- client/src/components/RoomActions.jsx
+++ client/src/components/RoomActions.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Card, CardActions, IconButton} from "@material-ui/core";
+import {Card, CardActions} from "@material-ui/core";
 import VoiceListener from "./VoiceListener";
 import PlaylistDialog from "./PlaylistDialog";
 import SpeechLogDialog from "./SpeechLogDialog";
@@ -9,15 +9,26 @@ class RoomActions extends React.Component {
         super(props);
         this.getVoiceCommands = this.getVoiceCommands.bind(this);
     }
+
     getVoiceCommands() {
         const playlists = this.props.roomPlaylists || [];
-        const commands = playlists.map(playlist => {
-            const phrases = playlist.phrases || [];
-            return phrases.map(phrase => ({
-                command: '(*) ' + phrase + ' (*)',
-                callback: () => this.props.playPlaylist(playlist.id)
-            }));
-        }).flat()
+        const phraseList = {};
+        const commands = playlists
+            .map(playlist => {
+                const phrases = playlist.phrases || [];
+                return phrases
+                    .filter(phrase => {
+                        if (phraseList[phrase]) return false;
+                        phraseList[phrase] = true;
+                        return true;
+                    })
+                    .map(phrase => ({
+                            command: '(*) ' + phrase + ' (*)',
+                            callback: () => this.props.playPlaylist(playlist.id)
+                        })
+                    )
+            })
+            .flat()
             .concat({
                 command: '*',
                 callback: (speech) => speech && this.props.receiveSpeech(speech)
@@ -30,7 +41,9 @@ class RoomActions extends React.Component {
             <React.Fragment>
                 <Card>
                     <CardActions>
-                        <PlaylistDialog playlists={this.props.playlists} saveRoomPlaylists={this.props.saveRoomPlaylists} chosenPlaylists={this.props.roomPlaylists}/>
+                        <PlaylistDialog playlists={this.props.playlists}
+                                        saveRoomPlaylists={this.props.saveRoomPlaylists}
+                                        chosenPlaylists={this.props.roomPlaylists}/>
                         <div style={{marginLeft: 'auto'}}/>
                         <SpeechLogDialog speechLog={this.props.speechLog}/>
                         <VoiceListener commands={this.getVoiceCommands()}/>
